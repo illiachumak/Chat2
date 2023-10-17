599d987117320c22b3f7d54b43eb7055148bf396
illiachumak <illia.chumak.2017@gmail.com>
2023-10-17 21:51:36 +0300

    deploy setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import replace from '@rollup/plugin-replace';
+import dotenv from 'dotenv';
 
-// https://vitejs.dev/config/
+const env = dotenv.config().parsed;
+
+const envToReplace = Object.keys(env).reduce((acc, key) => {
+  acc[`process.env.${key}`] = JSON.stringify(env[key]);
+  return acc;
+}, {});
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    replace(envToReplace)
+  ]
 })
